96118fd71dc80db2cd47bf6833bd65da50504933
abdulrahemfaqih <faqih3935@gmail.com>
2023-07-11 22:04:46 +0700

    update

--- public/js/script.js
+++ public/js/script.js
@@ -2,14 +2,22 @@ $(function () {
    $(".tombolTambahData").on("click", function () {
       $("#judulModal").html("Tambah Data Mahasiswa");
       $(".modal-footer button[type=submit]").html("Tambah Data");
+      $("#nama").val("");
+      $("#nrp").val("");
+      $("#email").val("");
+      $("#jurusan").val("");
+      $("#id").val("");
    });
 
    $(".tampilModalUbah").on("click", function () {
       $("#judulModal").html("Ubah Data Mahasiswa");
       $(".modal-footer button[type=submit]").html("Ubah Data");
-      $("#modal-body").attr("action", "http://localhost/belajarphp/phpmvc/public/mahasiswa/ubah");
+      $(".modal-body form").attr(
+         "action",
+         "http://localhost/belajarphp/phpmvc/public/mahasiswa/ubah"
+      );
 
-      const id = $(this).data('id');
+      const id = $(this).data("id");
 
       $.ajax({
          url: "http://localhost/belajarphp/phpmvc/public/mahasiswa/getUbah",
@@ -17,12 +25,13 @@ $(function () {
          method: "post",
          dataType: "json",
          success: function (data) {
-            $('#nama').val(data.nama)
-            $('#nim').val(data.nim)
-            $('#email').val(data.email)
-            $('#jurusan').val(data.jurusan)
-            $('#jurusan').val(data.jurusan)
-         }
+            $("#nama").val(data.nama);
+            $("#nim").val(data.nim);
+            $("#email").val(data.email);
+            $("#jurusan").val(data.jurusan);
+            $("#jurusan").val(data.jurusan);
+            $("#id").val(data.id);
+         },
       });
    });
 });
